fc8e0fd90092cab3e0e9e053a7d82d95e5e4bdfb
杨子旭 <578156479@qq.com>
2017-03-13 21:12:41 +0800

    Create maolv.c

---
+++ maolv.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+    //int n = 10; //n为年份，10年
+    //double p;  //增长倍数
+    //double r = 0.09;   //r为年增长率 
+    //p = pow( ( 1 + r ) , n );  //调用数学函数，意为(1+r)^n
+    //printf("10年后增长了 %lf 倍\n",p);  //输出语句
+    //
+    //return 0;  //返回语句
+    
+    
+    /*char c1,c2,c3,c4,c5;
+    c1='C';
+    c2='h';
+    c3='i';
+    c4='n';
+    c5='a';
+    printf("%c",c1+4);
+    printf("%c",c2+4);
+    printf("%c",c3+4);
+    printf("%c",c4+4);
+    printf("%c",c5+4);
+    return 0;*/
+
+
+
+    /*putchar(c1+4);
+    putchar(c2+4);
+    putchar(c3+4);
+    putchar(c4+4);
+    putchar(c5+4);
+    return 0;
+*/
